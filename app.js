ecc0c74acba222f9b0517a697ff950aeee450202
Tomoya <yasaschil.to12mo14@gmail.com>
2020-01-05 16:09:14 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict';
+// memoという名前のMapを作成（キーに順番、値に答えを格納）
+const memo = new Map();
+// 0番目、1番目の答えは予め定義されているので、格納。
+memo.set(0, 0);
+memo.set(1, 1);
+// もしMapがnをキーとした答えを持っていれば、その値をそのまま関数の値として返し（return memo.get(n);）、そうでない場合は再帰関数を計算して求めてMapに格納（memo.set）した後に返す（return value;）という処理。
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    if (memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n-1) + fib(n-2);
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n, value);
+    return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
